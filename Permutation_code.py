97f6e59349d79723e712cdc5d340613e766e7177
David92260 <dmonchaux@aol.com>
2017-02-12 12:49:48 +0100

    2nd commit

---
+++ Permutation_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2
+#-*- coding: utf-8 -*-
+
+"""
+Permutation encoding
+"""
+import os
+import numpy as np
+
+infile = "Test.txt"
+
+with open(infile, 'r') as f:
+	read_data = f.read()
+	print(read_data)
+
+
+
+
